c780bae6d688ff13d665292e694fc58d9992e29d
peterfoo22 <psuh22@gmail.com>
2023-02-11 15:20:29 +0700

    added images/style cards

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import image1 from './images/alexa.png';
+import image2 from './images/cortana.png';
+import image3 from './images/siri.png';
 
 function App() {
 return (
@@ -12,13 +15,14 @@ return (
             <ProfileCard
               handle="this is the first handle"
               title="first-title"
+              image={image1}
             />
           </div>
           <div className="column is-4">
-            <ProfileCard handle="second handle" title="second-title" />
+            <ProfileCard handle="second handle" title="second-title" image={image2} />
           </div>
           <div className="column is-4">
-            <ProfileCard handle="third handle" title="third title" />
+            <ProfileCard handle="third handle" title="third title" image={image3} />
           </div>
         </div>
       </section>
