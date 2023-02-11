f074a3dc8c3ecda526527d847a23a21a5dc3cc00
peterfoo22 <psuh22@gmail.com>
2023-02-11 15:03:47 +0700

    adding columns / card

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+  <div>
+    <div className="container">
+      <section className="section">
+        <div className="columns">
+          <div className="column is-4">
+            <ProfileCard
+              handle="this is the first handle"
+              title="first-title"
+            />
+          </div>
+          <div className="column is-4">
+            <ProfileCard handle="second handle" title="second-title" />
+          </div>
+          <div className="column is-4">
+            <ProfileCard handle="third handle" title="third title" />
+          </div>
+        </div>
+      </section>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
---
+++ first-react-app/src/ProfileCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProfileCard = ({title, handle, image}) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <div>Title is {title}</div>
+          <div>Handle is {handle}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard
